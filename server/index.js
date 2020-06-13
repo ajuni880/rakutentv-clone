e769389f784d21f8c97a2dd086052c9f7383b358
Junaid Aslam <ajunaid880@gmail.com>
2020-06-14 01:25:25 +0200

    updates

--- server/index.js
+++ server/index.js
@@ -11,5 +11,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log('Server is up');
+  console.log(`[app] server listening on port ${port}`);
 });
--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,30 @@
 const path = require('path');
 
+const htmlLoaderOptions =  {
+  attributes: {
+    list: [
+      {
+        tag: 'link',
+        attribute: 'href',
+        type: 'src',
+        filter: (tag, attribute, attributes) => {
+          if (!/icon/i.test(attributes.rel)) {
+            return false;
+          }
+          if (
+            attributes.type &&
+            attributes.type.trim().toLowerCase() !== 'image/png'
+          ) {
+            return false;
+          }
+
+          return true;
+        },
+      },
+    ]
+  }
+}
+
 module.exports = {
   resolve: {
     modules: [path.join(__dirname, 'src'), 'node_modules'],
@@ -12,6 +37,23 @@ module.exports = {
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+          options: htmlLoaderOptions
+        }
+      },
+      {
+        test: /\.(jpe?g|gif|png|svg)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'assets'
+          }
+        }
+      }
     ],
   },
 };
